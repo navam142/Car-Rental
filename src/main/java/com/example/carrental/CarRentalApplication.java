da3b82a9f73d9b1d97277518e895008e991dd506
Wundu Rebooted <navamsharma142@gmail.com>
2026-06-05 17:32:38 +0530

    refactor: update datasource properties and remove unused EnvConfig class

--- src/main/java/com/example/carrental/CarRentalApplication.java
+++ src/main/java/com/example/carrental/CarRentalApplication.java
@@ -1,5 +1,6 @@
 package com.example.carrental;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarRentalApplication {
 
     public static void main(String[] args) {
+
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory(".")
+                    .load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        System.getProperties().forEach((key, value) -> System.out.println(key + ": " + value));
         SpringApplication.run(CarRentalApplication.class, args);
     }
 
