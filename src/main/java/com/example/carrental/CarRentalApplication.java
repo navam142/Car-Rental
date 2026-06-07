1d6638c9cbdd428191c3663a34f4a44c4c4231c6
Wundu Rebooted <navamsharma142@gmail.com>
2026-06-07 21:22:00 +0530

    refactor: update docker-compose configuration for frontend and backend services

--- src/main/java/com/example/carrental/CarRentalApplication.java
+++ src/main/java/com/example/carrental/CarRentalApplication.java
@@ -4,21 +4,27 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CarRentalApplication {
 
     public static void main(String[] args) {
 
+        Logger logger =  Logger.getLogger(CarRentalApplication.class.getName());
+
         try {
             Dotenv dotenv = Dotenv.configure()
                     .directory(".")
+                    .ignoreIfMissing()
                     .load();
             dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            logger.log(Level.WARNING, "Could not load env variables", e);
             System.exit(1);
         }
-        System.getProperties().forEach((key, value) -> System.out.println(key + ": " + value));
+        System.getProperties().forEach((key, value) -> logger.info(key + ": " + value));
         SpringApplication.run(CarRentalApplication.class, args);
     }
 
